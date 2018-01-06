4adf36924c18e73b1ad7408d032eb9e9c5b5ffa8
waboli <waboli@126.com>
2018-01-07 03:23:00 +0800

    commit

--- src/main/java/com/isoftstone/hrms/config/DatabaseConfig.java
+++ src/main/java/com/isoftstone/hrms/config/DatabaseConfig.java
@@ -4,7 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@MapperScan是mybatis-spring中提供的一个注解,指定Mapper类所在的包
 @MapperScan("com.isoftstone.hrms.mapper")
 public class DatabaseConfig {
 
